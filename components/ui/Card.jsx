276103824c15b9e9c7da84d8deca7bf81b68243c
William James <wgj8860@gmail.com>
2025-08-10 17:18:54 +0800

    chore: remove unused components and UI utilities; keep only active space theme system

--- components/ui/Card.jsx
+++
@@ -1,9 +0,0 @@
-export default function Card({ className = '', children }) {
-  return (
-    <div
-      className={`rounded-xl border border-black/5 dark:border-white/10 bg-white/70 dark:bg-neutral-900/60 ${className}`}
-    >
-      {children}
-    </div>
-  );
-}
--- components/ui/index.js
+++
@@ -1,3 +0,0 @@
-export { default as Button } from '@/components/ui/Button';
-export { default as Card } from '@/components/ui/Card';
-export { default as Heading } from '@/components/ui/Heading';
